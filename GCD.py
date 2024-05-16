265a07ced2f58de5b4b2d2d6068c555f8c2468ae
jibrangill <jibran_online@hotmail.com>
2024-05-16 02:22:37 -0500

    gcd update to add comments

--- GCD.py
+++ GCD.py
@@ -1,15 +1,17 @@
 ####Program to calculate the GCD using Euclids Algorithm
 
 def gcd(a, b):
+    count = 0
     while (b != 0):
         t = a
-        print('assigning a to temporary variable,t = ',t)
+        print('assigning value of "a" to temporary variable,t = ',t)
         a = b
         print('a = b: ',a," = ",b)
         b = t % b
+        count +=1
         print('b = t mod b ->',b)
         print('return a ',a)
-    return a
-
-print(gcd(20, 8))
-print(gcd(60, 96))
+    return a, count
+result, iterations = gcd(200,12)
+print('GCD is: ', result, ' and loop ran ',iterations, ' times.')
+#print('GCD is: ', gcd(90, 96))
